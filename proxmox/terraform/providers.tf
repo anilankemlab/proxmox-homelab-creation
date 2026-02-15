0f4169dd7f9aeb332cdabe3dc8860768f07dd8f2
anilankem <anilankem@gmail.com>
2026-02-15 09:38:17 +0530

    all

--- proxmox/terraform/providers.tf
+++ proxmox/terraform/providers.tf
@@ -1,18 +1,18 @@
 terraform {
-    required_providers {
-        proxmox = {
-            source = "telmate/proxmox"
-        }
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.95.0"
     }
+  }
 }
 
+
 provider "proxmox" {
-  pm_api_url      = var.proxmox_api_url
-  pm_api_token_id = var.proxmox_token_id
-  pm_api_token_secret = var.proxmox_token_secret
+  endpoint = var.proxmox_api_url
 
-  # Set to true if you are using self-signed certificates and want to skip TLS verification.
-  pm_tls_insecure     = true
+  api_token = "${var.proxmox_token_id}=${var.proxmox_token_secret}"
 
+  insecure = true
 }
 

efe47788cf91220f8933b7068d7ecb0028ba179e
anilankem <anilankem@gmail.com>
2026-02-15 09:52:56 +0530

    same

--- proxmox/terraform/outputs.tf
+++ proxmox/terraform/outputs.tf
@@ -1,9 +1,14 @@
+output "vm_id" {
+  description = "The VM ID of the created VM"
+  value       = proxmox_virtual_environment_vm.vm_from_template.vm_id
+}
+
+output "vm_name" {
+  description = "The name of the created VM"
+  value       = proxmox_virtual_environment_vm.vm_from_template.name
+}
+
 output "vm_node" {
   description = "Proxmox node where VM is created"
   value       = proxmox_virtual_environment_vm.vm_from_template.node_name
-}
-
-output "vm_tags" {
-  description = "Tags assigned to VM"
-  value       = proxmox_virtual_environment_vm.vm_from_template.tags
 }

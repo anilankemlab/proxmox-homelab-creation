6b102ec0cbfe5bcf9dfb10bb482a64a1cd504b52
anilankem <29255999+anilankem@users.noreply.github.com>
2026-02-28 10:33:06 +0530

    Fix syntax for SSH key file inclusion

--- proxmox/terraform/main.tf
+++ proxmox/terraform/main.tf
@@ -54,7 +54,7 @@ resource "proxmox_virtual_environment_vm" "vm_from_template" {
       username = "root"
       password = "anilankem"
       keys = [
-        file("~/.ssh/id_ed25519.pub")
+        file("~/.ssh/id_ed25519.pub"),
         file("~/.ssh/mylab.pub")
       ]
     }
